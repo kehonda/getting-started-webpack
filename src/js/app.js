001e86cc17009f19405a7314053cb408cf001755
kehonda <731.kentaro@gmail.com>
2020-02-05 19:42:49 +0900

    moduleを利用したjs

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import add from './modules/add';
+import add from './modules/tax';
+
+const item1Price = 400;
+const item2Price = 600;
+const totalPrice = add(item1Price, item2Price);
+const salesTax = 1.08;
+const priceIncludeTax = tax(totalPrice, salesTax);
